44390c07d9b3d8f82f0ee9a519ce79a0bacd9539
FreakDove <bluedulgi1104@gmail.com>
2018-06-08 10:41:32 +0900

    Vive Input Test

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    private SteamVR_TrackedObject trackedobj;
+
+    private SteamVR_Controller.Device controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedobj.index); }
+    }
+
+    private void Awake()
+    {
+        trackedobj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void Update()
+    {
+        if(controller.GetHairTrigger())
+        {
+            
+        }
+    }
+}
---
+++ Assets/Script/Controller_Tes.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ContCube {Trigger=0, TriggerUD=1, Grip=2, GripUD=3, TouchPad=4}
+
+public class Controller_Tes : MonoBehaviour
+{
+
+	public MeshRenderer[] InputCubes;
+	private ContCube cubenum;
+	
+	private SteamVR_TrackedObject TrackedObj;
+
+	private SteamVR_Controller.Device controller
+	{
+		get { return SteamVR_Controller.Input((int) TrackedObj.index); }
+	}
+
+	private void Awake()
+	{
+		TrackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	private void Update()
+	{
+		InputCubes[(int) ContCube.TouchPad].transform.position = controller.GetAxis();
+		
+		if (controller.GetHairTrigger()) //트리거 누르고 있을 때
+		{
+			InputCubes[(int)ContCube.Trigger].material.color = Color.red;
+		}
+		else
+		{
+			InputCubes[(int)ContCube.Trigger].material.color = Color.white;
+		}
+
+		if (controller.GetHairTriggerDown()) //트리거를 눌렀을 때
+		{
+			InputCubes[(int)ContCube.TriggerUD].material.color = Color.red;
+		}
+
+		if (controller.GetHairTriggerUp()) //트리거를 올렸을 때 
+		{
+			InputCubes[(int)ContCube.TriggerUD].material.color = Color.white;
+		}
+
+		if (controller.GetPress(SteamVR_Controller.ButtonMask.Grip))
+		{
+			InputCubes[(int)ContCube.Grip].material.color = Color.red;
+		}
+		else
+		{
+			InputCubes[(int) ContCube.Grip].material.color = Color.white;
+		}
+
+		if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+		{
+			InputCubes[(int)ContCube.GripUD].material.color = Color.red;
+		}
+
+		if (controller.GetPressUp((SteamVR_Controller.ButtonMask.Grip)))
+		{
+			InputCubes[(int) ContCube.GripUD].material.color = Color.white;
+		}
+		
+		
+	}
+}
